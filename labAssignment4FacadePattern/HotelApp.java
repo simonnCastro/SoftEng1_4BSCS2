10b9caf5213ca58614f04defc0e6ea5fa5941a98
simonnCastro <61060425+simonnCastro@users.noreply.github.com>
2024-10-06 16:15:24 +0000

    new file:   labAssignment4FacadePattern/HotelApp.java

---
+++ labAssignment4FacadePattern/HotelApp.java
@@ -0,0 +1,17 @@
+public class HotelApp {
+    public static void main(String[] args) {
+        HotelService valetHotelService = new Valet();
+        FrontDesk valetFrontDesk = new FrontDesk(valetHotelService);
+
+        HotelService houseKeepingHotelService = new HouseKeeping();
+        FrontDesk houseKeepingFrontDesk = new FrontDesk(houseKeepingHotelService);
+
+        HotelService cartHotelService = new Cart();
+        FrontDesk cartFrontDesk = new FrontDesk(cartHotelService);
+
+        valetFrontDesk.pickUpVehicle("AAA 0000");
+        houseKeepingFrontDesk.cleanRoom("00");
+        cartFrontDesk.requestCart("00");
+
+    }
+}
